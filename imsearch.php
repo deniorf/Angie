2382b9f0f7b8bdfc82c19c6d5246439542d66942
deniorf <47238961+deniorf@users.noreply.github.com>
2019-02-01 02:34:50 -0500

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,234 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
+	<head>
+		<title>Buscar - Angie</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="DAVID" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.4.3 - www.websitex5.com" />
+		<meta property="og:locale" content="es" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://angielashesbrows.com/imsearch.php" />
+		<meta property="og:title" content="Buscar" />
+		<meta property="og:site_name" content="Angie" />
+		<meta property="og:image" content="http://angielashesbrows.com/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="400">
+		<meta property="og:image:height" content="400">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-4-3" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-4-3" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-4-3" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-4-3" media="screen" />
+		<link rel="stylesheet" type="text/css" href="imHeader_pluginAppObj_03/custom.css" media="screen, print" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?14-0-4-3-636845852016863265" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-4-3"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-4-3" data-files-version="14-0-4-3"></script>
+		<script type="text/javascript" src="imHeader_pluginAppObj_03/main.js"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.png?14-0-4-3-636845852016833274" type="image/png" />
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">Buscar - Angie</h1>
+						<div id="imHeaderObjects"><div id="imHeader_pluginAppObj_03_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_3" class="" > <div id="imHeader_imCellStyleGraphics_3"></div><div id="imHeader_imCellStyle_3" ><div id="imHeader_pluginAppObj_03">
+            <div id="soc_imHeader_pluginAppObj_03">
+                <div class="wrapper vertical flat shrink">
+                	<div class='social-icon flat'><a href='https://www.facebook.com/DAngie-Lashes-Brows-574796012985419/' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M57,93V54H70.14l2-15H57V29.09c0-4.39.94-7.39,7.24-7.39H72V8.14a98.29,98.29,0,0,0-11.6-.6C48.82,7.54,41,14.61,41,27.59V39H27V54H41V93H57Z"/></svg><span class='fallbacktext'>Fb</span></a></div><div class='social-icon flat'><a href='https://www.instagram.com/angie_lashes_brows/' target='_blank'><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M37,78.44c26.42,0,40.86-21.88,40.86-40.86,0-.62,0-1.24,0-1.86A29.22,29.22,0,0,0,85,28.29a28.67,28.67,0,0,1-8.25,2.26,14.41,14.41,0,0,0,6.32-7.94,28.78,28.78,0,0,1-9.12,3.49,14.37,14.37,0,0,0-24.47,13.1,40.77,40.77,0,0,1-29.6-15,14.37,14.37,0,0,0,4.45,19.17,14.26,14.26,0,0,1-6.5-1.8c0,0.06,0,.12,0,0.18A14.37,14.37,0,0,0,29.33,55.82a14.34,14.34,0,0,1-6.49.25A14.38,14.38,0,0,0,36.26,66a28.82,28.82,0,0,1-17.84,6.15A29.24,29.24,0,0,1,15,72a40.66,40.66,0,0,0,22,6.45"/></svg><span class='fallbacktext'>Tw</span></a></div>
+                </div>
+
+            </div>
+                <script type="text/javascript">
+                    socialicons_imHeader_pluginAppObj_03();
+                </script>
+        </div></div></div></div><div id="imHeader_imMenuObject_08_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_8" class="" > <div id="imHeader_imCellStyleGraphics_8"></div><div id="imHeader_imCellStyle_8" ><div id="imHeader_imMenuObject_08"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Home		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/services.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="services.html">
+Services		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/about.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="about.html">
+About		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_08',1000)});
+$(function () {$('#imHeader_imMenuObject_08 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_08-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_08-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imHeader_imObjectImage_10_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_10" class="" > <div id="imHeader_imCellStyleGraphics_10"></div><div id="imHeader_imCellStyle_10" ><img id="imHeader_imObjectImage_10" src="images/marca-de-agua.png" title="" alt="" />
+<script type="text/javascript">
+	x5engine.boot.push(function () {
+		 x5engine.imagefx.pulse('#imHeader_imObjectImage_10', 0.2, false); 
+	});
+</script>
+</div></div></div><div id="imHeader_imTextObject_11_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_11" class="" > <div id="imHeader_imCellStyleGraphics_11"></div><div id="imHeader_imCellStyle_11" ><div id="imHeader_imTextObject_11">
+	<div class="text-tab-content"  id="imHeader_imTextObject_11_tab0" style="">
+		<div class="text-inner">
+			<blockquote><div><pre data-placeholder="Traducción" data-fulltext=""><b><span class="fs22 cf1 ff1">BUSINESS HOURS</span></b></pre></div></blockquote><div><b><span class="fs22 cf1 ff1"> MONDAY TO FRIDAY</span></b></div><div><b><span class="fs22 cf1 ff1"> 9:00 am.-&gt; 5:30 pm.</span></b></div><div><b><span class="fs22 cf1 ff1"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SATURDAY</span></b></div><div><div><b><span class="fs22 cf1 ff1"> 9:00 am.-&gt; 3:30 pm.</span></b></div></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_1" class="" > <div id="imStickyBar_imCellStyleGraphics_1"></div><div id="imStickyBar_imCellStyle_1" ><div id="imStickyBar_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Home		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/services.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="services.html">
+Services		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/about.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="about.html">
+About		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_01',1000)});
+$(function () {$('#imStickyBar_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imStickyBar_imObjectImage_02_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_2" class="" > <div id="imStickyBar_imCellStyleGraphics_2"></div><div id="imStickyBar_imCellStyle_2" ><img id="imStickyBar_imObjectImage_02" src="images/Cool-Text---Angie-lashes-brows-313299368325107.png" title="" alt="" /></div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Resultados de búsqueda</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imTextObject_05_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_5" class="" > <div id="imFooter_imCellStyleGraphics_5"></div><div id="imFooter_imCellStyle_5" ><div id="imFooter_imTextObject_05">
+	<div class="text-tab-content"  id="imFooter_imTextObject_05_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: left;"><span class="fs10 cf1 ff1"><b>HOMEPAGE</b></span></div><div style="text-align: left;"><span class="fs10 cf1 ff1">Encontraras distintos </span></div><div style="text-align: left;"><span class="fs10 cf1 ff1">servicios </span><span class="fs10 cf1 ff1">que brindamos</span></div><div style="text-align: left;"><span class="fs10 cf1 ff1"></span><span class="fs10 cf1 ff1">al publico en general.</span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imObjectImage_06_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_6" class="" > <div id="imFooter_imCellStyleGraphics_6"></div><div id="imFooter_imCellStyle_6" ><img id="imFooter_imObjectImage_06" src="images/line_hor-1-.png" title="" alt="" /></div></div></div><div id="imFooter_imTextObject_13_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_13" class="" > <div id="imFooter_imCellStyleGraphics_13"></div><div id="imFooter_imCellStyle_13" ><div id="imFooter_imTextObject_13">
+	<div class="text-tab-content"  id="imFooter_imTextObject_13_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: right;" data-line-height="1" class="lh1"><a href="https://www.facebook.com/edwindavid.fasanandoruiz" onclick="return x5engine.utils.imPopUpWin('https://www.facebook.com/edwindavid.fasanandoruiz','imPopUp', 800,600);" class="imCssLink">by David</a></div><div style="text-align: right;" data-line-height="1" class="lh1"><span class="fs10 ff1"><a href="https://www.facebook.com/Developers-servicio-951922904939310/" onclick="return x5engine.imShowBox({ media:[{type: 'iframe', url: 'https://www.facebook.com/Developers-servicio-951922904939310/', width: 800, height: 600, description: 'Visita mi Pagina y estare pronto ayudarte'}]}, 0, this);" class="imCssLink">12denio@gmail.com</a></span></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imTextObject_15_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_15" class="" > <div id="imFooter_imCellStyleGraphics_15"></div><div id="imFooter_imCellStyle_15" ><div id="imFooter_imTextObject_15">
+	<div class="text-tab-content"  id="imFooter_imTextObject_15_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: left;"><span class="fs10 cf1 ff1"><b>ABOUT</b></span></div><div style="text-align: left;"><span class="fs10 cf1 ff1">Haremos que tus ojos</span></div><div style="text-align: left;"><span class="fs10 cf1 ff1">se vean</span><span class="fs10 cf1 ff1">herm osos con </span></div><div style="text-align: left;"><span class="fs10 cf1 ff1">nuestros diseños y</span></div><div style="text-align: left;"><span class="fs10 cf1 ff1">experiencia que tenemos.</span><br></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imTextObject_16_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_16" class="" > <div id="imFooter_imCellStyleGraphics_16"></div><div id="imFooter_imCellStyle_16" ><div id="imFooter_imTextObject_16">
+	<div class="text-tab-content"  id="imFooter_imTextObject_16_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: left;"><span class="fs10 cf1 ff1"><b>STORY</b></span></div><div style="text-align: left;"><span class="fs10 cf1 ff1"><b>7 años de experiencia nos a </b></span></div><div style="text-align: left;"><span class="fs10 cf1 ff1"><b>mostrado dar lo mejor de uno </b></span></div><div style="text-align: left;"><span class="fs10 cf1 ff1"><b>para que nuestro clientes esten </b></span></div><div style="text-align: left;"><span class="fs10 cf1 ff1"><b>satisfechos con nuestro trabajo.</b></span></div><div style="text-align: left;"><br></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imObjectForm_17_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_17" class="" > <div id="imFooter_imCellStyleGraphics_17"></div><div id="imFooter_imCellStyle_17" ><form id="imObjectForm_17" action="imemail/imEmailForm.php" method="post" enctype="multipart/form-data">
+	<fieldset class="first">
+		<div>
+			<div id="imObjectForm_17_1_container" class="imObjectFormFieldContainer"><label id="imObjectForm_17_1_label" for="imObjectForm_17_1">NAME</label><br /><div id="imObjectForm_17_1_field">
+<input type="text" class="" id="imObjectForm_17_1" name="imObjectForm_17_1" /></div>
+</div>
+			<div id="imObjectForm_17_2_container" class="imObjectFormFieldContainer"><label id="imObjectForm_17_2_label" for="imObjectForm_17_2">E-MAIL</label><br /><div id="imObjectForm_17_2_field">
+<input type="text" class="valEmail" id="imObjectForm_17_2" name="imObjectForm_17_2" /></div>
+</div>
+			<div id="imObjectForm_17_3_container" class="imObjectFormFieldContainer"><label id="imObjectForm_17_3_label" for="imObjectForm_17_3">MESSAGE</label><br /><div id="imObjectForm_17_3_field">
+<textarea class="" id="imObjectForm_17_3" name="imObjectForm_17_3" ></textarea></div>
+</div>
+			<div id="imObjectForm_17_4_container" class="imObjectFormFieldContainer"><label id="imObjectForm_17_4_label" for="imObjectForm_17_4">DATE</label><br /><div id="imObjectForm_17_4_field">
+<input type="text" data-showicon="true" data-format="[dd]/[mm]/[yyyy]" class="datepicker" id="imObjectForm_17_4" name="imObjectForm_17_4" /><img id="imObjectForm_17_4_icon" class="imDatePickerIcon" src="res/date.gif"></div>
+</div>
+		</div>
+	</fieldset>
+	<fieldset>
+		<input type="text" id="imObjectForm_17_prot" name="imSpProt" />
+	</fieldset>
+	<div id="imObjectForm_17_buttonswrap">
+		<input type="submit" value="Invia" />
+	</div>
+</form>
+<script>x5engine.boot.push('x5engine.imForm.initForm(\'#imObjectForm_17\', false, {jsid: \'6133C7E6DA6D2B911C93EABFF76EAC6C\', type: \'tip\', showAll: true, classes: \'validator\', landingPage: \'index.html\', labelColor: \'rgba(224, 224, 224, 1)\', fieldColor: \'rgba(0, 0, 0, 1)\', selectedFieldColor: \'rgba(0, 0, 0, 1)\'})');</script>
+</div></div></div><div id="imFooter_imObjectTitle_18_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_18" class="" > <div id="imFooter_imCellStyleGraphics_18"></div><div id="imFooter_imCellStyle_18" ><div id="imFooter_imObjectTitle_18"><span id ="imFooter_imObjectTitle_18_text" >Angie lashes brows</span > </div></div></div></div></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript.</div></noscript>
+	</body>
+</html>
